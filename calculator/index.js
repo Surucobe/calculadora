e1e56fd84cf55bce557a973927da8d503aded6f1
Suru <garudahorror25@gmail.com>
2023-11-13 11:35:38 -0400

    added new functions to render the results into the screen, save the original entry and put it in a second screen in order to see what was put and a new function to replace the eval interaction in order to not use it in the future

--- calculator/index.js
+++ calculator/index.js
@@ -1,5 +1,7 @@
 const numberButtons = document.querySelectorAll('.number-button');
 const operatorButtons = document.querySelectorAll('.operator-button');
+const initialScreen = document.querySelector('.current-operand');
+const previousOperationScreen = document.querySelector('.previous-operand');
 const result = document.querySelector('.result');
 
 let input = '';
@@ -8,14 +10,28 @@ numberButtons.forEach((elm) => elm.addEventListener('click', () =>{
   console.log(elm.innerHTML);
   input += (elm.innerHTML);
   console.log(input);
+  renderNumbersInScreen(elm.innerHTML);
 }));
 
 operatorButtons.forEach((elm) => elm.addEventListener('click', () => {
   console.log(elm.innerHTML);
   input += (elm.innerHTML);
   console.log(input);
+  renderNumbersInScreen(elm.innerHTML);
 }));
 
 result.addEventListener('click', () => {
-  console.log(eval(input));
+  renderResult(mathResult());
 });
+
+const mathResult = () => {
+  let result = input + ' = ' + eval(input);
+  return result;
+}
+
+const renderNumbersInScreen = (elm) => initialScreen.innerHTML += elm;
+function renderResult(elm) {
+  previousOperationScreen.innerHTML += input;
+  initialScreen.innerHTML = '';
+  initialScreen.innerHTML += elm;
+}
