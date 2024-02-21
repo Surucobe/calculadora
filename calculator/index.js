12f766414e680560bb48e5b5f65e21712f3bc3c2
suru <garudahorror25@gmail.com>
2024-02-21 12:33:52 -0400

    first working calculator, there is still things to do such as the replacement of the eval function and implemantation of the button to toggle the state of the first number into positive or negative

--- calculator/index.js
+++ calculator/index.js
@@ -1,55 +1,55 @@
-//Variables
-const numberButtons = document.querySelectorAll('.number-button');
-const operatorButtons = document.querySelectorAll('.operator-button');
-const initialScreen = document.querySelector('.current-operand');
-const previousOperationScreen = document.querySelector('.previous-operand');
-
-const deleteButton = document.querySelector('.delete-button');
-const cleanScreen = document.querySelector('.clear');
+//TODO: add a way to start with a negative number
+const display = document.getElementById('display');
+const $buttons = document.querySelectorAll('.operation');
 const result = document.querySelector('.result');
+const clear = document.querySelector('.clear');
+const deleteLastInput = document.querySelector('.delete-last');
+let finalResult = false;
 
-let input = '';
+const addValueToTheInput = (input) => {
+  display.value += input;
+}
 
-//functions
-numberButtons.forEach((elm) => elm.addEventListener('click', () =>{
-  input += (elm.innerHTML);
-  console.log(input);
-  renderChangesInScreen(initialScreen, elm.innerHTML);
+const checkValidInput = (btn) => {
+  if(finalResult == true && !isNaN(btn.innerHTML)) return false;
+  if(display.value.length < 1 && isNaN(btn.innerHTML)) return false;
+  if(display.value.match(/[+-\/*]$/gm) && isNaN(btn.innerHTML)) return false;
+  if(display.value.split(/[^0-9.]/).slice(-1)[0].includes('.') && btn.innerHTML == '.'){
+    return false;
+  } else {
+    finalResult = false;
+  }
+  return true;
+}
+
+$buttons.forEach(btn => btn.addEventListener('click', () => {
+  if(checkValidInput(btn)){
+    addValueToTheInput(btn.innerHTML)
+  } else {
+    return
+  }
 }));
 
-operatorButtons.forEach((elm) => elm.addEventListener('click', () => {
-  input += (elm.innerHTML);
-  console.log(input);
-  renderChangesInScreen(initialScreen, elm.innerHTML);
-}));
-
-cleanScreen.addEventListener('click', () =>{
-  input = '';
-  renderChangesInScreen(previousOperationScreen);
-  renderChangesInScreen(initialScreen);
-});
-
-deleteButton.addEventListener('click', () =>{
-  input = input.slice(0, -1);
-  renderChangesInScreen(initialScreen);
-  renderChangesInScreen(initialScreen, input);
-});
-
-result.addEventListener('click', () => {
-  renderResult(mathResult());
-});
-
-const mathResult = () => {
-  let result = input + ' = ' + eval(input);
-  return result;
+//TODO: change in order to make sure to not use the eval function, must create the proper functions to evaluate and make the corresponding operations
+const operate = () => {
+  finalResult = true;
+  let resultToDisplay = Number(eval(display.value));
+  if(resultToDisplay % 1 === 0){
+    display.value = resultToDisplay;
+  } else {
+    display.value = resultToDisplay.toFixed(2);
+  }
 }
 
-const renderChangesInScreen = (container, elm) => {
-  elm ? container.innerHTML += elm : container.innerHTML = '';
+const deleteLastInputInDisplay = () => {
+  display.value = display.value.slice(0, -1)
 }
 
-function renderResult(elm) {
-  previousOperationScreen.innerHTML += input;
-  initialScreen.innerHTML = '';
-  initialScreen.innerHTML += elm;
-}
+result.addEventListener('click', operate);
+
+clear.addEventListener('click', () => {
+  finalResult = false;
+  display.value = '';
+})
+
+deleteLastInput.addEventListener('click', () => deleteLastInputInDisplay())
