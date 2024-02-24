4a40d060a0ec606669f093b35eb6dfaf2a31ebb1
suru <garudahorror25@gmail.com>
2024-02-23 22:24:01 -0400

    work on the logic and behaviour once the calculator delivers a result, still need to improve a few aspects that i haven't even think of myself

--- calculator/index.js
+++ calculator/index.js
@@ -12,42 +12,54 @@ let operation = '';
 let nextValue = '';
 
 const addValueToInput = (input) => {
-  //TODO: add the logic to evaluate and asign the corresponding values to each variable and reset the input
+  //TODO: improve the code tomake it more clean
   if(!isNaN(input) || input == '.'){
-    //FIX
     if(lastInput == 'operator' && operation.length < 1){
       operation = display.value;
       display.value = ''
       lastInput = 'number';
     }
   } else {
+    debugger
     if(lastInput == 'number' && !nextValue){
       if(!currentValue){
         currentValue = display.value;
       }
+      //TODO: work on the behavior once a new operator is asign and there currently is a final result
+      if(currentValue && operation){
+        operate();
+        return
+      }
       display.value = ''
       lastInput = 'operator'
-      //TODO: work on the behaviour of this funtion in order to better manipulate the results on screen
-      if(currentValue && operation) operate()
     }
   }
 
-  //TODO: work on where and when to display
+  //TODO: work on where and when to display (?)
   display.value += input;
 }
 
-//TODO: readjust in order to work with a single string, which is the way it will be operate later on
-//figure if i will fraction this onto multiple functions to work as one
+//function to check if an input is a number or a dot
+function isNumberOrDot(elm){
+  if(!isNaN(elm) || elm == '.'){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//TODO: figure if i will fraction this onto multiple functions to work as one
 const checkValidInput = (btn) => {
+  const input = btn.innerHTML;
   //prevents the user from pushing a number or a dot input if there is a current final result
   //FIX: allows user to put a dot in the final result
-  if(finalResult == true && !isNaN(btn.innerHTML)) return false;
+  if(finalResult == true && isNumberOrDot(input)) return false;
   //prevents the user from input a operator or a dot before a number if there are no numbers yet
-  if(display.value.length < 1 && isNaN(btn.innerHTML)) return false;
+  if(display.value.length < 1 && isNaN(input)) return false;
   //prevents the user from pushin an operator input such as +-/* if there is already one
-  if(display.value.match(/[+-\/*]$/gm) && isNaN(btn.innerHTML)) return false;
+  if(display.value.match(/[+-\/*]$/gm) && isNaN(input)) return false;
   //prevents the user from adding a second dot if there is already a number with a dot on it, meaning a float or decimal
-  if(display.value.split(/[^0-9.]/).slice(-1)[0].includes('.') && btn.innerHTML == '.'){
+  if(display.value.includes('.') && input == '.'){
     return false;
   } else {
     finalResult = false;
@@ -76,7 +88,6 @@ const divide = () => {
   return Number(currentValue) / Number(nextValue);
 }
 
-//TODO: change in order to make sure to not use the eval function, must create the proper functions to evaluate and make the corresponding operations
 const operate = () => {
   finalResult = true;
   nextValue = display.value;
@@ -99,27 +110,34 @@ const operate = () => {
       break;
   }
 
+  nextValue = '';
+  operation = '';
+
   if(currentValue % 1 === 0){
     display.value = currentValue;
-    nextValue = ''
-    operation = ''
   } else {
     display.value = currentValue.toFixed(2);
-    nextValue = '';
-    operation = '';
   }
 }
 
 const deleteLastInputInDisplay = () => {
-  //TODO: agregar condicional para manejar el comportamiento en caso de que haya habido un resultado final
-  display.value = display.value.slice(0, -1);
+  if(finalResult == true){
+    clearDisplay();
+  }else{
+    display.value = display.value.slice(0, -1);
+  }
 }
 
 result.addEventListener('click', operate);
 
-clear.addEventListener('click', () => {
+function clearDisplay() {
   finalResult = false;
+  currentValue = '';
+  operation = '';
+  nextValue = '';
   display.value = '';
-})
+}
 
-deleteLastInput.addEventListener('click', () => deleteLastInputInDisplay())
+clear.addEventListener('click', clearDisplay)
+
+deleteLastInput.addEventListener('click', deleteLastInputInDisplay)
