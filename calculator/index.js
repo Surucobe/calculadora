c2e657a9b3094d5933866147cf7440af60fe2b6b
suru <garudahorror25@gmail.com>
2024-02-27 15:31:53 -0400

    Officially finished the logic behind the calculator currently observating the addition of a feature unrelated to the main logic and pourpose along with changing the look of the calculator to be more aesthetic

--- calculator/index.js
+++ calculator/index.js
@@ -1,5 +1,3 @@
-//TODO: add a way to start with a negative number
-//TODO: create functions that pushes either number or operator and changes the last input
 const display = document.getElementById('display');
 const $buttons = document.querySelectorAll('.operation');
 const result = document.querySelector('.result');
@@ -7,10 +5,12 @@ const clear = document.querySelector('.clear');
 const deleteLastInput = document.querySelector('.delete-last');
 const negativeNumber = document.querySelector('.negative');
 let finalResult = false;
+//true for numbers and false for operations
 let lastInput = true;
 let currentValue = '';
 let operation = '';
 let nextValue = '';
+let isNegative = false;
 
 //function to check if an input is a number or a dot
 function isNumberOrDot(elm){
@@ -19,14 +19,14 @@ function isNumberOrDot(elm){
   } else {
     return false;
   }
-}
+};
 
+//function to handle the toggle of the last input
 function toggleLastInput(elm){
   lastInput = isNumberOrDot(elm)
-}
+};
 
 const addValueToInput = (input) => {
-  debugger
   //TODO: improve the code to make it more clean
   //this one evaluate when the input is a number or a dot
   if(isNumberOrDot(input)){
@@ -38,13 +38,14 @@ const addValueToInput = (input) => {
       display.value = '';
       toggleLastInput(input);
     }
+  //this section evaluates when the input is not a number or a dot
   } else {
-    //this one evaluates when the input is not a number or a dot
+    //asign the current value on display to the current value variable
     if(lastInput == true){
       if(!currentValue){
         currentValue = display.value;
       }
-      //TODO: work on the behavior once a new operator is asign and there currently is a final result
+      //section in charge to handle if a new operator has been pass and there is a pending operation
       if(currentValue && operation){
         operate();
         operation = input;
@@ -55,8 +56,6 @@ const addValueToInput = (input) => {
       toggleLastInput(input)
     }
   }
-
-  //TODO: work on where and when to display (?)
   display.value += input;
 }
 
@@ -64,20 +63,19 @@ const addValueToInput = (input) => {
 const checkValidInput = (btn) => {
   const input = btn.innerHTML;
   //prevents the user from pushing a number or a dot input if there is a current final result
-  if(finalResult == true && input == '.') return false;
+  if(finalResult == true && input == '.') {
+    return false;
+  } else if(finalResult == true && isNumberOrDot(input) && display.value === '0'){
+    return false;
+  }
   //prevents the user from input a operator or a dot before a number if there are no numbers yet
   if(display.value.length < 1 && isNaN(input)) return false;
   //prevents the user from pushin an operator input such as +-/* if there is already one
   if(display.value.match(/[+-\/*]$/gm) && isNaN(input)) return false;
   //prevents the user from adding a second dot if there is already a number with a dot on it, meaning a float or decimal
-  if(display.value.includes('.') && input == '.'){
-    return false;
-  } else {
-    //TODO: figure out why i did this
-    // finalResult = false;
-  }
+  if(display.value.includes('.') && input == '.') return false;
   return true;
-}
+};
 
 $buttons.forEach(btn => btn.addEventListener('click', () => {
   if(checkValidInput(btn)){
@@ -100,19 +98,23 @@ const divide = () => {
   return Number(currentValue) / Number(nextValue);
 }
 
-function transformNumber() {
-  let valueReturned;
+function handleResult() {
+  let returnedValue;
   if(currentValue % 1 === 0){
-    valueReturned = currentValue;
+    returnedValue = currentValue;
   } else {
-    valueReturned = currentValue.toFixed(2);
+    returnedValue = currentValue.toFixed(2);
   }
-  return valueReturned;
-}
+  return returnedValue;
+};
 
 const operate = () => {
   nextValue = display.value;
-  debugger
+  if(operation == '/' && nextValue === '0'){
+    display.value = 'Syntax Error'
+    setTimeout(() => alert(`Attempt to divide by ${nextValue}`), '100')
+    return
+  }
   switch (operation) {
     case '+':
       currentValue = sum();
@@ -127,22 +129,23 @@ const operate = () => {
       currentValue = divide();
       break
   
-    default:
-      break;
+    default: 
+      alert('Operation is not complete');
+      return;
   }
 
   nextValue = '';
+  operation = '';
   
   if(finalResult == false){
-    display.value = transformNumber();
-    operation = '';
+    display.value = handleResult();
     finalResult = true;
   }else{
-    display.value = transformNumber();
-    toggleLastInput();
+    display.value = handleResult();
+    toggleLastInput(display.value);
   }
   
-}
+};
 
 const deleteLastInputInDisplay = () => {
   if(finalResult == true){
@@ -150,7 +153,7 @@ const deleteLastInputInDisplay = () => {
   }else{
     display.value = display.value.slice(0, -1);
   }
-}
+};
 
 result.addEventListener('click', operate);
 
@@ -160,8 +163,22 @@ function clearDisplay() {
   operation = '';
   nextValue = '';
   display.value = '';
+};
+
+function handleNegativeOpetion() {
+  if(finalResult == true) return;
+  if(display.value.length < 1) return;
+
+  if(!isNegative){
+    isNegative = !isNegative;
+    display.value = '-' + display.value.slice(0);
+  }else{
+    isNegative = !isNegative;
+    display.value = display.value.slice(1);
+  }
 }
 
-clear.addEventListener('click', clearDisplay)
+negativeNumber.addEventListener('click', handleNegativeOpetion)
+clear.addEventListener('click', clearDisplay);
 
-deleteLastInput.addEventListener('click', deleteLastInputInDisplay)
+deleteLastInput.addEventListener('click', deleteLastInputInDisplay);
